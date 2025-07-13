46b467a339b8b794a17a94cf08f60c8163ccaa89
Sameer0166 <sameerpasha0166@gmail.com>
2025-07-13 21:02:03 +0530

    its my fourth commit

---
+++ ternary_operator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int age;
+printf("enter the age of the person: ");
+scanf("%d",&age);
+age>=18 ? printf("the given pereson is adult"):printf("the given person is not adult");
+return 0;
+}
