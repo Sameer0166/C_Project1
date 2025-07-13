0594333e29e8125321795331eb5c430df55df697
Sameer0166 <sameerpasha0166@gmail.com>
2025-07-13 23:00:04 +0530

    its my first commit

--- sunday.c
+++ sunday.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main() {
+    /*
+        this program is on operator "AND" "&&"
+        here 1 = true 
+        and  0= false    \
+        here "issunday =1" means it is sunday   
+        here "issnowing =0" means not snowing
+    */
     int issunday=1,issnowing=0;
     printf("%d",issunday&&issnowing);
     return 0;
