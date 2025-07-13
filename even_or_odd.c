ed8c5dd083e644745e14928eae6a198bd90b22b1
Sameer0166 <sameerpasha0166@gmail.com>
2025-07-13 22:47:43 +0530

    its my first commit

--- even_or_odd.c
+++ even_or_odd.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 int main(){
+    /*
+    This program checks if a number is even or odd.
+    It prompts the user to enter a number and then checks if the number is even or odd      
+    using the modulus operator (%).
+    If the number is even (i.e., divisible by 2), it prints "the given number is even".
+    If the number is odd (i.e., not divisible by 2), it prints "the given number is odd".   
+    This program is useful for understanding how to use the modulus operator in C to determine the parity of a number.  
+    */
     int num;
 printf("Enter a number:");
 scanf("%d", &num);
