a5615ebd3ae2d05e7f14406386f77489b0f93479
Sameer0166 <sameerpasha0166@gmail.com>
2025-07-13 22:54:23 +0530

    its my first commit

--- monday.c
+++ monday.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main()
 {
+    /*
+        this program is on operator "OR" "||"
+        here 1 = true 
+        and  0= false    */
 int ismonday=1,itsraining=1;
 printf("%d",ismonday||itsraining);
 return 0;
