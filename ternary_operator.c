435dcdb91f29e78a494f9fddefe3e19acdef6948
Sameer0166 <sameerpasha0166@gmail.com>
2025-07-13 23:01:58 +0530

    its my first commut

--- ternary_operator.c
+++ ternary_operator.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main()
 {
+    /*
+    This program checks the age of a person and determines their eligibility to vote.
+    It prompts the user to enter their age and then uses a ternary operator to check if the person is an adult or not.
+    If the age is 18 or older, it prints "the given person is adult".
+    If the age is less than 18, it prints "the given person is not adult".
+    This program is useful for understanding how to use the ternary operator in C to evaluate conditions and execute different statements based on the result.
+    */
 int age;
 printf("enter the age of the person: ");
 scanf("%d",&age);
