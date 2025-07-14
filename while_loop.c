7e4971a5f384a6b7bdf6c0556cdb26bee576bb7f
Sameer0166 <sameerpasha0166@gmail.com>
2025-07-15 00:08:40 +0530

    its my first commit

---
+++ while_loop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i=1; // initializing the loop variable
+    while(i<=6) // condition for the while loop
+    {
+        printf("%d\n",i); // printing the current value of i
+        i++; // incrementing the loop variable or updation 
+   }
+ return 0; 
+}
