bb756e8b40c724e4601315f4a6d1e500e83627f3
Sameer0166 <sameerpasha0166@gmail.com>
2025-07-14 23:38:28 +0530

    its my first commit

---
+++ post_decrement.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int i=5;
+    printf("%d\n",i--);
+    printf("%d",i);
+    /*    This program demonstrates the use of the post-decrement operator in C.
+    It initializes an integer variable 'i' to 5, prints its value using the post    
+decrement operator, and then prints the value of 'i' again after the decrement.
+    The post-decrement operator (i--) decreases the value of 'i' by 1       
+after its current value has been used in the expression.
+    This is a simple demonstration of how to use the post-decrement operator in C.
+    The program is useful for understanding how the post-decrement operator works,
+    */
+    return 0;
+}     
---
+++ prefix_decrement.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int j=5;
+    printf("%d\n",--j);
+    printf("%d",j);
+    /* This program demonstrates the use of the pre-decrement operator in C.
+    It initializes an integer variable 'j' to 1, prints its value using the pre
+    decrement operator, and then prints the value of 'j' again after the decrement.
+    The pre-decrement operator (--j) decreases the value of 'j' by 1
+    before its current value is used in the expression.
+    This is a simple demonstration of how to use the pre-decrement operator in C.
+    The program is useful for understanding how the pre-decrement operator works,
+    */
+    return 0;
+
+}
