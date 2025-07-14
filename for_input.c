c2137b4e71ac030755907ccabc935e4aa0adce3f
Sameer0166 <sameerpasha0166@gmail.com>
2025-07-15 00:39:46 +0530

    its my first commit

---
+++ for_input.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int m,n;
+    printf("enter the m value");
+    scanf("%d",&m);
+    printf("enter the n value");
+    scanf("%d",&n);
+for( int i=0;i<=n;i++){
+printf("%d\n",i);
+   /*
+    This program uses a for loop to print numbers starting from 0 up to 'n'.
+    The for loop initializes the variable 'i' to 0 and continues to execute the loop
+    as long as 'i' is less than or equal to 'n'.
+    In each iteration, it prints the current value of 'i' and increments 'i' by 1.
+    This is a simple demonstration of how to use a for loop in C to repeat a block of code multiple times.
+    The program is useful for understanding the basic structure of a for loop, including initialization, condition
+   */
+
+}
+return 0;
+}
