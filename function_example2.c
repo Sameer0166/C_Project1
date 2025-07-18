2d7552c7e22e71c525b389111aa0e6f56b3dfe32
Sameer0166 <sameerpasha0166@gmail.com>
2025-07-18 23:36:30 +0530

    this is my first commit

---
+++ function_example2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void indian();
+void french();
+int main()
+{
+    char ch,f;
+    printf("enter the f for french and i for indian:");
+    scanf("%c",&ch);
+
+    if(ch=='f')
+    {
+       french();
+    }
+   
+    else if (ch=='i')
+    {
+         indian();
+        
+    }
+    else if(ch!='i'&&ch!='f'){
+printf("enter i or f\nenter the valid country");
+    }
+    else
+    {
+        printf("enter the valid country");
+    }
+  
+    return 0;
+}
+void indian(){
+    printf("namaste\n");  
+}
+void french(){
+    printf("bonjour\n");
+}
